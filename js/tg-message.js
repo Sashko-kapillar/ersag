f8f92a85a951362165648d00d4b1f7cdd35b3f3c
sashko-kapillar <kapillarua@gmail.com>
2024-11-24 17:27:14 +0200

    mod tg-message

--- js/tg-message.js
+++ js/tg-message.js
@@ -1,5 +1,3 @@
-// import axios from "axios";
-
 const TOKEN = "7387360375:AAHk8V7iqtSG7ZQrFcR3KquXvmB5xUcGNAw";
 const CHAT_ID = "-1002343393160";
 const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
@@ -12,11 +10,27 @@ document.getElementById("tg-message").addEventListener("submit", function (e) {
   message += `<b>Ім'я відправника: </b> ${this.name.value}\n`;
   message += `<b>Номер телефону: </b> ${this.phone.value}\n`;
   message += `<b>Текст повідомлення: </b> ${this.comment.value}`;
-  console.log(message);
 
-  axios.post(URI_API, {
-    chat_id: CHAT_ID,
-    parse_mode: "html",
-    text: message,
-  });
+  axios
+    .post(URI_API, {
+      chat_id: CHAT_ID,
+      parse_mode: "html",
+      text: message,
+    })
+    .then((res) => {
+      this.name.value = "";
+      this.phone.value = "";
+      this.comment.value = "";
+      iziToast.info({
+        message: "Повідомлення успішно відправилося...",
+        position: "topCenter",
+        color: "green",
+      });
+    })
+    .catch((err) => {
+      console.warn(err);
+    })
+    .finally(() => {
+      console.log("Finish");
+    });
 });
