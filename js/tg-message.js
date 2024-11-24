b6c860a4f422d31642a039f5de5944a0f1252d59
sashko-kapillar <kapillarua@gmail.com>
2024-11-24 16:25:23 +0200

    mod tg-message

--- js/tg-message.js
+++ js/tg-message.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 
 const TOKEN = "7387360375:AAHk8V7iqtSG7ZQrFcR3KquXvmB5xUcGNAw";
 const CHAT_ID = "-1002343393160";
@@ -10,7 +10,9 @@ document.getElementById("tg-message").addEventListener("submit", function (e) {
 
   let message = `<b>Замовлення з сайту</b>\n`;
   message += `<b>Ім'я відправника: </b> ${this.name.value}\n`;
-  message += `<b>Номер телефону: </b> ${this.phone.value}`;
+  message += `<b>Номер телефону: </b> ${this.phone.value}\n`;
+  message += `<b>Текст повідомлення: </b> ${this.comment.value}`;
+  console.log(message);
 
   axios.post(URI_API, {
     chat_id: CHAT_ID,
