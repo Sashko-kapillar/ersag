9e1b55b197cff996f417182951f600998596a7b9
sashko-kapillar <kapillarua@gmail.com>
2024-11-24 09:46:03 +0200

    mod tg-message

--- js/tg-message.js
+++ js/tg-message.js
@@ -1,6 +1,13 @@
-const TOKEN = "";
-const CHAT_ID = "";
+const TOKEN = "7387360375:AAHk8V7iqtSG7ZQrFcR3KquXvmB5xUcGNAw";
+const CHAT_ID = "-1002343393160";
 
 document.getElementById("tg-message").addEventListener("submit", function (e) {
   e.preventDefault();
+  console.log("Oky");
+
+  let message = `<b>Замовлення з сайту</b>\n`;
+  message += `<b>Ім'я відправника: </b> ${this.name.value}\n`;
+  message += `<b>Номер телефону: </b> ${this.phone.value}`;
+
+  console.log(message);
 });
